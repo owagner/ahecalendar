a9bfae8db578eea8422d0164c112e4488b02bc57
Oliver Wagner <owagner@tellerulam.com>
2015-08-13 13:54:18 +0200

    - add --todayornext

--- ahecalendar.py
+++ ahecalendar.py
@@ -3,7 +3,9 @@
 # Download des AHE-Abfallkalenders im JSON-Forma 
 #
 # Gibt entweder die Rohdaten aus, oder mit der Option --next
-# die jeweils nächsten Abfuhrtermine pro Containertyp
+# die jeweils nächsten Abfuhrtermine pro Containertyp, oder mit
+# --todayornext auch am Tag des Aufrufs stattfindende
+# Abfuhren
 #
 
 from __future__ import print_function
@@ -19,6 +21,7 @@ parser.add_argument("plz",metavar="PLZ",type=int,help="Postleitzahl")
 parser.add_argument("street",metavar="Straße",help="Straße")
 parser.add_argument("no",metavar="Hausnummer",type=int,help="Hausnummer")
 parser.add_argument("--next",action='store_true',help="Nächste Abfuhren ausgeben")
+parser.add_argument("--todayornext",action='store_true',help="Nächste Abfuhren ausgeben (inkl. Heute)")
 
 args=parser.parse_args()
 
@@ -32,12 +35,16 @@ if data["status"]!=1:
 # Kalender holen
 req={"v":1,"district_id":data["id"]}
 cal=json.loads(urllib2.urlopen("http://ahe-afk.app.remondis.de/schedule?"+urllib.urlencode(req)).read())
-if args.next:
+if args.next or args.todayornext:
 	now=time.time()
 	percontainer={}
 	# Test
 	for s in cal:
 		when=time.mktime(time.strptime(s["scheduled_date"].split(" ")[0],"%Y-%m-%d"))
+		if args.todayornext:
+			# Make cutoff end-of-day
+			# Merrily ignore DST changes or leap seconds while watching Jerome's face twitch
+			when+=60*60*24-1
 		if when>now:
 			cont=s["container_long_name"]
 			if not cont in percontainer or percontainer[cont]>when:
